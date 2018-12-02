c37fcff0dd26361307db07dd39a21316bd1b516a
Alexander Claudino Daffara <alexanderdaffara@gmail.com>
2018-12-02 00:24:38 -0800

    successfully imported torch

--- src/main.py
+++ src/main.py
@@ -1,3 +1,12 @@
+import numpy
+import torch
+import torch.autograd as autograd
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+torch.manual_seed(1)
+
 print("hello world")
 print("world hello")
 print(".")
