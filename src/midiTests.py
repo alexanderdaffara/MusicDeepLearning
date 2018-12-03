e18c99b0f530e17031253642e2f84fb4aced1c9b
achan <achan@100.88.71.48>
2018-12-02 21:22:21 -0800

    Music21 Tests

---
+++ src/midiTests.py
@@ -0,0 +1,8 @@
+from music21 import *
+
+n = note.Note("Ab3")
+n.duration.type = 'quarter'
+#n.show()
+
+littleMelody = converter.parse("tinynotation: 3/4 g4 a b c d b8 g a2")
+littleMelody.show('midi')
