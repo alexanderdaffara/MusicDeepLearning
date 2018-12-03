082604f2f773304289bc184aa638c8e164ec7fe9
Alexander Claudino Daffara <alexanderdaffara@gmail.com>
2018-12-03 15:30:27 -0800

    added learner

--- src/pytorchTests.py
+++ src/pytorchTests.py
@@ -1,6 +1,7 @@
 import numpy
 import torch
 import torch.autograd as autograd
+from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
