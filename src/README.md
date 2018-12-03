deedfd949f5fa1f772aa85c88cddbef4d198c70f
Absenta11 <Absenta11@users.noreply.github.com>
2018-12-03 15:28:35 -0800

    Added README, started on main.py

---
+++ src/README.md
@@ -0,0 +1,28 @@
+
+hLSTM, mLSTM = setUpLSTMs(magicNumbers)
+
+for( every file ):
+    reset hidden layers
+    inputArr1, inputArr2 = convertFileToInputArray()
+
+    #CUDA/Graphics Card speed-up?
+
+    trainHarmonyLSTM( hLSTM, inputArray2)
+    trainMelodyLSTM( mLSTM, inputArray1, inputArray2)
+
+noteArr1 = ""
+noteArr2 = ""
+
+for i in range(tickLimit):
+    #Returns noteArr2 appended with new prediction using hLSTM
+    noteArr2 = printHarmony(hLSTM, noteArr2)
+
+for i in range(tickLimit):
+    noteArr1 = printMelody(mLSTM, noteArr2, noteArr1)
+
+playMIDI(noteArr1, noteArr2)
+
+# convertFileToInputArr == vectorization of music
+# trainMelodyLSTM() == how to factor in chords?
+
+ Ideas: If multiple vectorizations, GANy thing; Music2Vec
--- src/main.py
+++ src/main.py
@@ -1,33 +1,12 @@
-"""
+from fileinput import filename
+import numpy
+import torch
+import torch.autograd as autograd
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from music21 import *
 
-hLSTM, mLSTM = setUpLSTMs(magicNumbers)
-
-for( every file ):
-    reset hidden layers
-    convertFileToInputArray()
-
-    #CUDA/Graphics Card speed-up?
-
-    trainHarmonyLSTM( hLSTM, inputArray2)
-    trainMelodyLSTM( mLSTM, inputArray1, inputArray2)
-
-noteArr1 = ""
-noteArr2 = ""
-
-for i in range(tickLimit):
-    #Returns noteArr2 appended with new prediction using hLSTM
-    noteArr2 = printHarmony(hLSTM, noteArr2)
-
-for i in range(tickLimit):
-    noteArr1 = printMelody(mLSTM, noteArr2, noteArr1)
-
-playMIDI(noteArr1, noteArr2)
-
-# convertFileToInputArr == vectorization of music
-# trainMelodyLSTM() == how to factor in chords?
-
- Ideas: If multiple vectorizations, GANy thing; Music2Vec
-
-"""
-
-
+def convertFileToMIDIArr(filename):
+    
+    
