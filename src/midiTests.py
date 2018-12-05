7e184a923a19e010f2a0449b5c79fd8ee593e2c9
Absenta11 <Absenta11@users.noreply.github.com>
2018-12-04 23:02:13 -0800

    Modified midiTests to test output methods

--- src/midiTests.py
+++ src/midiTests.py
@@ -3,13 +3,49 @@ from music21 import *
 #s = stream.Stream()
 #mf = midi.translate.streamToMidiFile(s)
 #mf.open('/MIDIs/happy.mid', 'wb')
+"""
+s = stream.Stream()
+mf = midi.translate.streamToMidiFile(s)
 
-mf.open("../MIDIs/output.mid")
-mf.write()
 mt = midi.MidiTrack(1)
+
+dt = midi.MidiEvent(mt)
+dt.type = 'DeltaTime'
+dt.time = 0
+dt.channel = None
+
 me1 = midi.MidiEvent(mt)
 me1.type = 'NOTE_ON'
 me1.channel = 3
 me1.time = 200
 me1.pitch = 60
 me1.velocity = 120
+
+dt1 = midi.MidiEvent(mt)
+dt1.type = 'DeltaTime'
+dt1.time = 200
+dt1.channel = None
+
+me2 = midi.MidiEvent(mt)
+me2.type = 'NOTE_OFF'
+me2.channel = 3
+me2.time = 200
+me2.pitch = 60
+me2.velocity = 120
+
+mt.events.append(me1)
+mt.events.append(dt)
+mt.events.append(me2)
+mt.events.append(dt1)
+mf.tracks.append(mt)
+
+print(mf)
+
+mf.open("../MIDIs/output.mid", 'wb')
+mf.write()
+mf.close
+"""
+
+s1 = stream.Stream()
+#s1.show('midi')
+s1.write('midi', "../MIDIs/output.mid")
