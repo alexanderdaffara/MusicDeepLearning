Fixed NaN issue with nonlinearity function (softmax) between hidden states, trained LSTMs with mini-epoch 4

src/MyLSTM.py
@@ -4,12 +4,66 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import torch.nn.utils.clip_grad as clip
+
 
 class LSTMPredictor(nn.Module):
 
 
     def __init__(self, input_dim, hidden_dim, output_dim):
         super(LSTMPredictor, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+
+        # The LSTM takes word embeddings as inputs, and outputs hidden states
+        # with dimensionality hidden_dim.
+        self.lstm = nn.LSTM(input_dim, hidden_dim)
+        self.hidden2out = nn.Linear(hidden_dim, self.output_dim)
+        self.hidden = self.init_hidden()
+
+    def init_hidden(self):
+        # Before we've done anything, we dont have any hidden state.
+        # Refer to the Pytorch documentation to see exactly
+        # why they have this dimensionality.
+        # The axes semantics are (num_layers, minibatch_size, hidden_dim)
+        return (torch.zeros(1, 1, self.hidden_dim),
+                torch.zeros(1, 1, self.hidden_dim))
+
+    def forward(self, inputVec):
+        #print("inputVec =")
+        #print(inputVec)
+        
+        inputVec = torch.FloatTensor(inputVec).view(1, 1, -1)
+        
+        #print(inputVec)
+        
+        lstm_out, self.hidden = self.lstm(
+            inputVec, self.hidden)
+        
+        """
+        print("lstm_out, hidden: ")
+        print(lstm_out)
+        print(self.hidden)
+        """
+                
+        out_space = F.softmax(lstm_out, dim=2)
+
+        out_space = self.hidden2out(out_space).view(1, 1, self.output_dim)
+        
+        #print(out_space)
+        #sub_list = torch.clone(out_space[:, :, :12])
+        #sub_list = F.softmax(sub_list, dim=2)
+        #new_out = torch.cat([ sub_list, out_space[:, :, 12:] ], dim = 2)
+        #print(new_out)
+        #clip.clip_grad_value_(out_space, .5)
+        
+        return out_space
+    
+class LSTMPredictorSoftmax(nn.Module):
+
+
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(LSTMPredictorSoftmax, self).__init__()
         self.hidden_dim = hidden_dim
         self.output_dim = output_dim
 
@@ -29,25 +83,33 @@ class LSTMPredictor(nn.Module):
                 torch.zeros(1, 1, self.hidden_dim))
 
     def forward(self, inputVec):
-        #print(inputVec)
+        
         inputVec = torch.FloatTensor(inputVec).view(1, 1, -1)
         
         lstm_out, self.hidden = self.lstm(
             inputVec, self.hidden)
-        out_space = self.hidden2out(lstm_out).view(1, 1, self.output_dim)
         
-        #print(out_space)
-        #sub_list = torch.clone(out_space[:, :, :12])
-        #sub_list = F.softmax(sub_list, dim=2)
-        #new_out = torch.cat([ sub_list, out_space[:, :, 12:] ], dim = 2)
-        #print(new_out)
+        out_space = F.softmax(lstm_out, dim=2)
+
+        out_space = self.hidden2out(out_space).view(1, 1, self.output_dim)
+        
+        new_out = F.softmax(out_space, dim=2)
         
         return new_out
     
-def prepareLSTM(inputDim, hiddenDim, outputDim):
-    model = LSTMPredictor(inputDim, hiddenDim, outputDim)
+def prepareLSTM(inputDim, hiddenDim, outputDim, isSoftMax):
+    
+    if (isSoftMax == True):
+        model = LSTMPredictorSoftmax(inputDim, hiddenDim, outputDim)
+        #print("softmax input")
+        #print(inputDim)
+    else:
+        model = LSTMPredictor(inputDim, hiddenDim, outputDim)
+        #print("not softmax inputDim")
+        #print(inputDim)
+        
     loss_function = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=.1)
+    optimizer = optim.SGD(model.parameters(), lr=.01)
     return (model, loss_function, optimizer)
 
 def trainLSTM(output_dim, model, loss_function, optimizer, training_data, epochs):
@@ -63,9 +125,9 @@ def trainLSTM(output_dim, model, loss_function, optimizer, training_data, epochs
     
     for epoch in range(epochs):
         
+        #print(epoch)
         
         for i in range(len(training_data) - 1):
-            
             
             # Step 1. Remember that Pytorch accumulates gradients.
             # We need to clear them out before each instance
@@ -77,11 +139,15 @@ def trainLSTM(output_dim, model, loss_function, optimizer, training_data, epochs
     
             # Step 2. Get our inputs ready for the network, that is, turn them into
             # Tensors of word indices.
-            input = torch.FloatTensor(training_data[i])
-            target = torch.FloatTensor(training_data[i+1]).view(1, 1, output_dim)
+            input = torch.FloatTensor(training_data[i]).view(1, 1, model.output_dim)
+            target = torch.FloatTensor(training_data[i+1]).view(1, 1, model.output_dim)
+            #print(target)
     
             # Step 3. Run our forward pass.
+            #print(input)
             prediction = model(input)
+            #print(prediction)
+            #print(prediction)
             #print(input)
             #print(target)
             #print(prediction)
@@ -90,6 +156,9 @@ def trainLSTM(output_dim, model, loss_function, optimizer, training_data, epochs
             # Step 4. Compute the loss, gradients, and update the parameters by
             #  calling optimizer.step()
             loss = loss_function(target, prediction)
+            
+            
+            #print(loss)
             loss.backward()
             optimizer.step()
 

src/midiPrinter.py
@@ -13,31 +13,61 @@ from music21 import *
 import math
 
 
-training_data = []
-with open("../Intermediates/training_data", "rb") as fp:
-    training_data = pickle.load(fp)
+pitch_data = []
+with open("../Intermediates/pitch_data", "rb") as fp:
+    pitch_data = pickle.load(fp)
     
-mLSTM = torch.load("../Intermediates/mLSTM")
+rhythm_data = []
+with open("../Intermediates/rhythm_data", "rb") as fp:
+    rhythm_data = pickle.load(fp)
+ 
+    
+mpLSTM = torch.load("../Intermediates/mpLSTMtmp")
+mrLSTM = torch.load("../Intermediates/mrLSTMtmp")
 
+"""
 inputArr = []
 oneHotArr = [0 for r in range(12)]
 idx = np.random.randint(0,11)
-oneHotArr[idx] = 100
-rest = [0,0,0,0]
+oneHotArr[idx] = 1
+rest = [0,0, 0, 0]
 for i in range(4):
     rest[i] = np.random.randint(0, 100)
 inputArr = inputArr + [oneHotArr + rest]
-inputArr[0] = mLSTM(inputArr[0]).tolist()[0][0]
+"""
+nextPitch = pitch_data[4][6]
+nextRhythm = rhythm_data[3][0]
 
+#print(nextPitch)
+#print(nextRhythm)
+
+nextPitch = mpLSTM(nextPitch).tolist()[0][0]
+print(nextRhythm)
+nextRhythm = mrLSTM(nextRhythm).tolist()[0][0]
+#print(nextRhythm)
+
+
+pitchArr = []
+rhythmArr = []
+pitchArr.append(nextPitch)
+rhythmArr.append(nextRhythm)
 #print()
 for i in range(100):
     #print(i)
     #print(inputArr[i])
-    next = mLSTM(inputArr[i]).tolist()[0][0]
-    next[12] = math.floor(next[12])
-    next[13] = math.floor(next[13])
-    inputArr.append( next )
-    #print(next)
-
+    print(mpLSTM(pitchArr[i]).tolist()[0][0])
+    nextPitch = mpLSTM(pitchArr[i]).tolist()[0][0]
+    nextRhythm = mrLSTM(rhythmArr[i]).tolist()[0][0]
+    pitchArr.append(nextPitch)
+    rhythmArr.append(nextRhythm)
+"""
+[0.017690028995275497, -0.023935995995998383, -0.054671451449394226, -0.003906804136931896,
+     0.036537908017635345, 0.0303180068731308, 0.035989824682474136 -0.04869161918759346,
+    -0.016102591529488564, 0.03156878426671028, -0.009204618632793427, 0.027298886328935623, nan, nan]
+[0.015781491994857788, -0.024790210649371147, -0.05739596486091614, -0.0017393557354807854,
+ 0.03929348662495613, 0.027984388172626495, 0.03854452446103096, -0.048154011368751526,
+  -0.015895375981926918, 0.02896677888929844, -0.010697830468416214, 0.029026683419942856]
+"""
 #print(inputArr)
-midiFunctions.printToMidi(inputArr)
+
+midiFunctions.printToMidi(pitchArr, rhythmArr)

src/trainer.py
@@ -10,17 +10,32 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-training_data = []
-with open("../Intermediates/training_data", "rb") as fp:
-    training_data = pickle.load(fp)
+mpDim = 12
+mrDim = 2
 
-print(training_data)
-
-(mLSTM, loss_function, optimizer) = MyLSTM.prepareLSTM(16, 512, 16)
-
-for i in range(len(training_data)):
+pitch_data = []
+with open("../Intermediates/pitch_data", "rb") as fp:
+    pitch_data = pickle.load(fp)
     
-    print("Training on Song %d\n" % i)
-    MyLSTM.trainLSTM(16, mLSTM, loss_function, optimizer, training_data[i], 15)
+rhythm_data = []
+with open("../Intermediates/rhythm_data", "rb") as fp:
+    rhythm_data = pickle.load(fp)
 
-torch.save(mLSTM, "../Intermediates/mLSTM")
+(mpLSTM, loss_function, optimizer) = MyLSTM.prepareLSTM(12, 1024, 12, True)
+(mrLSTM, loss_function, optimizer) = MyLSTM.prepareLSTM(2, 1024, 2, False)
+
+for i in range(len(pitch_data)):
+    
+    print("Training on Song %d\n" % (i))
+    
+    #print(rhythm_data[i])
+    MyLSTM.trainLSTM(mpDim, mpLSTM, loss_function, optimizer, pitch_data[i], 4)
+    MyLSTM.trainLSTM(mrDim, mrLSTM, loss_function, optimizer, rhythm_data[i], 4)
+    
+    if(i % 10 == 0):
+        torch.save(mpLSTM, "../Intermediates/mpLSTMtmp")
+        torch.save(mrLSTM, "../Intermediates/mrLSTMtmp")
+        print("saved!")
+
+torch.save(mpLSTM, "../Intermediates/mpLSTM")
+torch.save(mrLSTM, "../Intermediates/mrLSTM")